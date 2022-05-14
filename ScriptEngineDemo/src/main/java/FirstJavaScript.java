5e696a419e06ae95108faa9b8d636ade87cb3fa4
wangsz05 <wangsz05@126.com>
2022-05-14 15:23:44 +0800

    NashornSandbox

--- ScriptEngineDemo/src/main/java/FirstJavaScript.java
+++ ScriptEngineDemo/src/main/java/FirstJavaScript.java
@@ -1,32 +1,45 @@
+import com.alibaba.fastjson.JSONObject;
+import delight.nashornsandbox.NashornSandbox;
+import delight.nashornsandbox.NashornSandboxes;
+import netscape.javascript.JSObject;
+
 import javax.script.*;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
 
 import static java.lang.System.*;
 
 public class FirstJavaScript {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws ScriptException {
         ScriptEngineManager manager = new ScriptEngineManager();
         // 得到javascript脚本引擎
 
         ScriptEngine engine = manager.getEngineByName("javascript");
         String rce = "new java.lang.ProcessBuilder['(java.lang.String[])'](['cmd ',' /c ','calc']).start()";
-        try {
-            // 开始运行脚本，并返回当前的小时
-            Double hour = (Double) engine.eval(rce);
-            String msg;
-            // 将小时转换为问候信息
-            if (hour < 10) {
-                msg = "上午好";
-            } else if (hour < 16) {
-                msg = "下午好";
-            } else if (hour < 20) {
-                msg = "晚上好";
-            } else {
-                msg = "晚安";
-            }
-            out.printf("小时%s: %s%n", hour, msg);
-        } catch (ScriptException e) {
-            err.println(e);
-        }
+        String rce1 = "\"\".getClass().forName(\"java.lang.Runtime\").getMethod(\"getRuntime\").invoke(null),\"calc.exe\")";
+//        try {
+//
+//           engine.eval(rce);
+//
+//        } catch (ScriptException e) {
+//            err.println(e);
+//        }
+
+        String jsf1 = "function test1() {return \"hello world\" }";
+
+    NashornSandbox sandbox = NashornSandboxes.create();
+    sandbox.setMaxCPUTime(100);// 设置脚本执行允许的最大CPU时间（以毫秒为单位），超过则会报异常,防止死循环脚本
+    sandbox.setMaxMemory(1024 * 1024); //设置JS执行程序线程可以分配的最大内存（以字节为单位），超过会报ScriptMemoryAbuseException错误
+    sandbox.allowNoBraces(false); // 是否允许使用大括号
+    sandbox.allowLoadFunctions(true); // 是否允许nashorn加载全局函数
+    sandbox.setMaxPreparedStatements(30); // because preparing scripts for execution is expensive // LRU初缓存的初始化大小，默认为0
+    sandbox.setExecutor(Executors.newSingleThreadExecutor());// 指定执行程序服务，该服务用于在CPU时间运行脚本
+        sandbox.allow(ProcessBuilder.class);
+
+
+        sandbox.eval(jsf1);
     }
+
 }
